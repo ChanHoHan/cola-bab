7df84333241ac4d1fb9832e333abf6bf66de3f59
rkskekzzz <wkdlfflxh@naver.com>
2022-02-07 01:05:12 +0900

    Feat : preferMode

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,34 @@ import * as React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Main, Select, Result } from './Pages';
 import './App.css';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 
 function App() {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode]
+  );
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/:id" element={<Select />} />
-        <Route path="/:id/result" element={<Result />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/:id" element={<Select />} />
+          <Route path="/:id/result" element={<Result />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 export default App;
