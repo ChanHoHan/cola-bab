7a71581ad84491b9fffad1e1940aca70525180ad
chanho <chhqm2759@naver.com>
2022-02-06 23:28:10 +0900

    Feat : backend project init

---
+++ backend/src/main/java/cola/colabab/ColaBabApplication.java
@@ -0,0 +1,13 @@
+package cola.colabab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColaBabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColaBabApplication.class, args);
+	}
+
+}
